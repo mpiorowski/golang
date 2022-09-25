b9c7f06b11c14fa6ce43aff7e218ce123cd29205
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-09-25 13:22:55 +0200

    fix

--- homeit/homeit_grpc.pb.go
+++ homeit/homeit_grpc.pb.go
@@ -35,7 +35,7 @@ func NewFoodServiceClient(cc grpc.ClientConnInterface) FoodServiceClient {
 
 func (c *foodServiceClient) GetSupplies(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Supplies, error) {
 	out := new(Supplies)
-	err := c.cc.Invoke(ctx, "/proto.FoodService/GetSupplies", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/homeit.FoodService/GetSupplies", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -80,7 +80,7 @@ func _FoodService_GetSupplies_Handler(srv interface{}, ctx context.Context, dec 
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.FoodService/GetSupplies",
+		FullMethod: "/homeit.FoodService/GetSupplies",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(FoodServiceServer).GetSupplies(ctx, req.(*Empty))
@@ -92,7 +92,7 @@ func _FoodService_GetSupplies_Handler(srv interface{}, ctx context.Context, dec 
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var FoodService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "proto.FoodService",
+	ServiceName: "homeit.FoodService",
 	HandlerType: (*FoodServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
