888cf5f939443b3ee4014cc150bcffd9d6d377a1
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-09-25 13:28:13 +0200

    update

--- homeit/gateway_grpc.pb.go
+++ homeit/gateway_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.21.6
-// source: homeit.proto
+// source: gateway.proto
 
 package homeit
 
@@ -101,5 +101,5 @@ var FoodService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "homeit.proto",
+	Metadata: "gateway.proto",
 }
--- homeit/proto.sh
+++ homeit/proto.sh
@@ -1,4 +1,4 @@
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    ./homeit.proto
+    ./gateway.proto
 
