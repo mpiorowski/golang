45043d116ddeba2ce040f8aff562a49923705908
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-10-16 20:38:49 +0200

    update

--- main.go
+++ main.go
@@ -3,8 +3,28 @@ package utils
 import (
 	"log"
 	"os"
-
+	"time"
 )
+
+func ContainsString(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func RoundUpTime(t time.Time, roundOn time.Duration) time.Time {
+	t = t.Round(roundOn)
+
+	if time.Since(t) >= 0 {
+		t = t.Add(roundOn)
+	}
+
+	return t
+}
 
 func MustGetenv(k string) string {
 	v := os.Getenv(k)
